2ef53d6c1c1ff4e5c893f2cb5ed9b425e74024e7
Yevhenii007 <yevhenii.zhuravskyi.xt.2014@gmail.com>
2020-06-08 23:14:36 +0300

    some changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
-function Person(firstName, lastName){
+function Person(firstName, lastName) {
 	this.firstName = firstName;
 	this.lastName = lastName
-	this.showData = function(){
-			console.log(`Hello ${this.firstName} ${this.lastName}`)
+	this.showData = function () {
+		console.log(`Hello ${this.firstName} ${this.lastName}`)
 	}
 }
 let newP = new Person('Ivan', 'Ivanov');
--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,49 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cssnano = require('gulp-cssnano');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
-const rename = require("gulp-rename");
-const image = require('gulp-image');
-const browserSync = require('browser-sync').create();
+const project_folder = "build";
+const source_folder = "app";
 
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const rename = require("gulp-rename");
+const image = require("gulp-image");
+const browserSync = require("browser-sync").create();
+const fileinclude = require("gulp-file-include");
+const del = require("del");
 
 const paths = {
 	styles: {
-			src: 'app/styles/**/*.scss',
-			dest: 'build/css'
+		src: "app/styles/**/*.scss",
+		dest: "build/css",
 	},
 	scripts: {
-			src: 'app/scripts/**/*.js',
-			dest: 'build/scripts'
+		src: "app/scripts/**/*.js",
+		dest: "build/scripts",
 	},
 	html: {
-			src: 'app/**/*.html',
-			dest: 'build/'
+		src: [source_folder + "/**/*.html", "!" + source_folder + "/**/_*.html"],
+		dest: "build/",
 	},
 	images: {
-			src: 'app/images/*.*',
-			dest: 'build/images'
-	}
+		src: "app/images/*.*",
+		dest: "build/images",
+	},
+	clean: {
+		src: "./" + project_folder + "/",
+	},
 };
-
 
 function browser(done) {
 	browserSync.init({
-			server: {
-					baseDir: './build'
-			},
-			port: 3000
+		server: {
+			baseDir: "./build",
+		},
+		port: 3000,
 	});
 	done();
-};
-
+}
 
 function browserReload(done) {
 	browserSync.reload();
@@ -46,34 +51,47 @@ function browserReload(done) {
 }
 
 function styles() {
-	return gulp.src(paths.styles.src)
-			.pipe(sass().on('error', sass.logError))
-			.pipe(cssnano())
-			.pipe(autoprefixer({ browsers: ['last 3 versions', '> 1%', 'ie 8', 'ie 7'] }))
-			.pipe(rename({ suffix: '.min' }))
-			.pipe(gulp.dest(paths.styles.dest))
-			.pipe(browserSync.stream())
+	return gulp
+		.src(paths.styles.src)
+		.pipe(sass().on("error", sass.logError))
+		.pipe(cssnano())
+		.pipe(
+			autoprefixer({
+				browsers: ["last 3 versions", "> 1%", "ie 8", "ie 7"],
+			})
+		)
+		.pipe(
+			rename({
+				suffix: ".min",
+			})
+		)
+		.pipe(gulp.dest(paths.styles.dest))
+		.pipe(browserSync.stream());
 }
 
 function scripts() {
-	return gulp.src(paths.scripts.src)
-			.pipe(concat('main.min.js'))
-			.pipe(uglify())
-			.pipe(gulp.dest(paths.scripts.dest))
-			.pipe(browserSync.stream())
+	return gulp
+		.src(paths.scripts.src)
+		.pipe(concat("main.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.scripts.dest))
+		.pipe(browserSync.stream());
 }
 
 function html() {
-	return gulp.src(paths.html.src)
-			.pipe(gulp.dest(paths.html.dest))
-			.pipe(browserSync.stream())
+	return gulp
+		.src(paths.html.src)
+		.pipe(fileinclude())
+		.pipe(gulp.dest(paths.html.dest))
+		.pipe(browserSync.stream());
 }
 
 function images() {
-	return gulp.src(paths.images.src)
-			.pipe(image())
-			.pipe(gulp.dest(paths.images.dest))
-			.pipe(browserSync.stream())
+	return gulp
+		.src(paths.images.src)
+		.pipe(image())
+		.pipe(gulp.dest(paths.images.dest))
+		.pipe(browserSync.stream());
 }
 
 function watch() {
@@ -81,9 +99,14 @@ function watch() {
 	gulp.watch(paths.scripts.src, scripts);
 	gulp.watch(paths.html.src, html);
 	gulp.watch(paths.images.src, images);
-	gulp.watch('./app/*.html', gulp.series(browserReload));
+	gulp.watch("./app/*.html", gulp.series(browserReload));
 }
 
-const build = gulp.parallel(styles, scripts, html, images);
-gulp.task('build', build);
-gulp.task('default', gulp.parallel(watch, browser, build));
+function clean() {
+	return gulp
+		.del(path.clean);
+}
+
+const build = gulp.series(gulp.parallel(styles, scripts, html, images));
+gulp.task("build", build);
+gulp.task("default", gulp.parallel(watch, browser, build));
